d8fa447108c2f6bbdb7306cb7d40c33c858a5b7a
59013-96-09 <59013-96-09@exchange.ad.afpanet>
2023-07-03 13:44:58 +0200

    work connexion

--- ctrl/execute.php
+++ ctrl/execute.php
@@ -1,15 +1,42 @@
+
 <?php
 
-include "../M/DBManager.class.php";
+session_start();
 
-$db = new DBManager();
+// include "../M/DBManager.class.php";
+
+// $client = new DBManager();
 
 
-$nom = $_POST['prenom'];
-$prenom = $_POST['nom'];
-$sex = $_POST['sex']; 
+// $id = $_POST['prenom'];
+// $prenom = $_POST['nom'];
+// $sex = $_POST['sex']; 
 
-$db->insertEmploye($nom, $prenom, $sex);
+// $db->insertEmploye($nom, $prenom, $sex);
 
-//print_r($db->selectListeEmploye());
+// //print_r($db->selectListeEmploye());
+
+
+if (isset($_POST['submit'])) 
+{ 
+    $id = $_POST ['id'];
+    $mdp = $_POST ['mdp'];
+
+    $db = new PDO ('mysql:host=localhost;dbname=hotel;charset=utf8mb4', 'root', '');
+
+    $sql = "SELECT * FROM utilisateur where Login = '$id' ";
+    $result = $db->prepare($sql);
+    $result->execute();
+
+    if ($result->rowCount() > 0)
+    {
+        $data = $result->fetchAll();
+        if (password_verify($mdp, $data[$i], ["mot_De_Passe"]))
+        {
+            echo "Connexion effectuée, bien ouej Leïla !";
+            $_SESSION["Login"] = $id;
+        }
+    }
+
+}
 ?>
--- view/connexion.php
+++ view/connexion.php
@@ -1,19 +1,22 @@
+<?php session_start(); ?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Exemple de formulaire</title>
+        <title>Connexion</title>
         <link href = "./style.connexion.css" rel = "stylesheet">
     </head>
     <body>
-        <form class="form" action="../ctrl/execute.php" method="get" >
+        <form class="form" action="../ctrl/execute.php" method="POST">
             <div class="">
                 <label for="id">Identifiant : </label>
-                <input id="id" name="id" type="text" />
+                <input id="id" name="id" type="text" placeholder = "Entrez votre identifiant"/>
             </div>
             <div class="">
                 <label for="mdp">Mot de passe : </label>
-                <input id="mdp" name="mdp" type="text" />
+                <input id="mdp" name="mdp" type="text" placeholder = "Entrez votre mot de passe"/>
             </div>
             <input type="submit" id="submit" name="submit" value="Envoyer" />
         </form>
