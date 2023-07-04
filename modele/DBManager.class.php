ca88a17842c7ab9051eecf539fbc556c02fd06df
Aliiel <leila.mouaci@gmail.com>
2023-07-04 13:55:59 +0200

    maj connexion

--- modele/DBManager.class.php
+++ modele/DBManager.class.php
@@ -15,31 +15,33 @@
            
             if (isset($_POST['submit'])) 
             { 
-            $id = $_POST ['id'];
-            $mdp = $_POST ['mdp'];
+                $id = $_POST ['id'];
+                $mdp = $_POST ['mdp'];
 
-            $db = new PDO ('mysql:host=localhost;dbname=hotel;charset=utf8mb4', 'root', '');
+                $db = new PDO ('mysql:host=localhost;dbname=hotel;charset=utf8mb4', 'root', '');
 
-            $sql = "SELECT * FROM utilisateur where Login = '$id' ";
-            $result = $db->prepare($sql);
-            $result->execute();
+                $sql = "SELECT * FROM utilisateur where Login = '$id' and mot_De_Passe = '$mdp'";
+                $result = $db->prepare($sql);
+                $result->execute();
 
             if ($result->rowCount() > 0)
 
-         {
-            $data = $result->fetchAll();
-            // if (password_verify($mdp, $data["mot_De_Passe"]))
-            // {
-            echo "Connexion effectuée, bien ouej Leïla !";
-            $_SESSION["Login"] = $id;
+            {
+                $bdd = $result->fetchAll();
+
+            if (password_verify($mdp, $bdd["mot_De_Passe"]))
+            
+            {
+                echo "Connexion effectuée, bien ouej Leïla !";
+                $_SESSION["Login"] = $id;
             }
-        // else {
-
-        // }
     
-}
+            }
+            }
         }
+    }
 
+?>
    
   
 
@@ -53,8 +55,5 @@
 
         // }
 
-    }
 
 
-
-?>
