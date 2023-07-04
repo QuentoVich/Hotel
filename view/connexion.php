1daf63d0fafcb99f4eaa4f0184453eebe55bd2b8
Quentin Olivier <quentin.olivier6@outlook.fr>
2023-07-04 15:05:39 +0200

    front connexion

--- view/connexion.php
+++ view/connexion.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Connexion</title>
-        <link href = "./style.connexion.css" rel = "stylesheet">
-    </head>
-    <body>
-        <form class="form" action="../ctrl/execute.php" method="POST">
-            <div class="">
-                <label for="id">Identifiant : </label>
-                <input id="id" name="id" type="text" placeholder = "Entrez votre identifiant"/>
-            </div>
-            <div class="">
-                <label for="mdp">Mot de passe : </label>
-                <input id="mdp" name="mdp" type="password" placeholder = "Entrez votre mot de passe"/>
-            </div>
-            <input type="submit" id="submit" name="submit" value="Envoyer" />
-        </form>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Connexion</title>
+    <link href="./style.connexion.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <form class="form" action="../ctrl/execute.php" method="POST">
+        <div id="titre">
+            <h1>Connexion</h1>
+        </div>
+        <DIV id = "corps_form">
+        <div class="element">
+            <label for="id">Identifiant : </label>
+            <input id="id" name="id" type="text" placeholder="Entrez votre identifiant" />
+        </div>
+        <div class="element">
+            <label for="mdp">Mot de passe : </label>
+            <input id="mdp" name="mdp" type="password" placeholder="Entrez votre mot de passe" />
+        </div>
+        <input type="submit" id="submit" name="submit" value="Envoyer" />
+        </DIV>
+    </form>
+</body>
+
 </html>
