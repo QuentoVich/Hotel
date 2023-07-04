2b646200d397b0490e2e0119c8ebd824c9d9098a
Quentin Olivier <quentin.olivier6@outlook.fr>
2023-07-04 13:21:28 +0200

    Reservation OK

--- modele/DBManager.class.php
+++ modele/DBManager.class.php
@@ -1,86 +1,71 @@
 <?php
 
-	class DBManager
-    {   private $bdd;
-        private static $count_reservation = 1;
-       
-
-        //constructeur qui initialise la connxion à la BDD
-        public function __construct()
-        {
-            $this->bdd = new PDO('mysql:host=localhost;dbname=hotel;charset=utf8', 'root', '');
+class DBManager
+{
+    private $bdd;
+    // private static $count_reservation = 1;
 
 
-        }
-
-        public function getMaxReservationNumber()
+    //constructeur qui initialise la connxion à la BDD
+    public function __construct()
     {
-        $sql = "SELECT MAX(Num_Reservation) FROM reservation";
+        $this->bdd = new PDO('mysql:host=localhost;dbname=hotel;charset=utf8', 'root', '');
+    }
+
+
+    //methode qui ajoute une reservation
+
+    public function setReservation($date_Reservation, $date_Entree, $date_Sortie, $Code_Client, $Num_Chamb): void
+    {
+        $sql = "INSERT INTO reservation (date_Reservation, date_Entrée, date_Sortie, Code_Client, Num_Chamb) 
+            VALUES (?, ?, ?, ?, ?)";
         $stmt = $this->bdd->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetchColumn();
+        $stmt->execute([$date_Reservation, $date_Entree, $date_Sortie, $Code_Client, $Num_Chamb]);
 
-        if ($result === false) {
-            return 1; // Si aucune réservation n'existe, on commence à 1
-        }
+        // Récupérer le numéro max de réservation généré automatiquement
+        $sql = "SELECT MAX(num_Reservation) FROM reservation";
+        $stmt = $this->bdd->query($sql);
+        $num_Reservation = $stmt->fetchColumn();
 
-        return $result + 1;
-    }
-   
-        //methode qui ajoute une reservation
-      
-        public function setReservation( $date_Reservation, $date_Entree, $date_Sortie, $Code_Client, $Num_Chamb): void { 
-            
-            $num_Reservation = self::$count_reservation++;
-
-                // Afficher le numéro de réservation
-                echo "Reservation faites avec succès \n Numéro de réservation : " . $num_Reservation;
-            
-            $sql = "INSERT INTO reservation (num_Reservation , date_Reservation, date_Entrée, date_Sortie, Code_Client, Num_Chamb) 
-            VALUES (?, ?, ?, ?, ?, ?)";
-            $stmt = $this->bdd->prepare($sql);
-            $stmt->execute([$num_Reservation, $date_Reservation, $date_Entree, $date_Sortie, $Code_Client, $Num_Chamb]);
-
-           
-        }
-
-
-
-
-        // //methode qui supprime un employe par son noemp
-        // public function supprEmploye($noemp) : void {
-
-        // }
-
-        // //methode qui mets à jour le salaire d'un amployé
-        // public function updateSalaireEmploye($noemp, $sal) : void {
-
-        // }
-
-
-        /**
-         * Get the value of count_reservation
-         */ 
-        public function getCount_reservation()
-        {
-                return $this->count_reservation;
-        }
-
-        /**
-         * Set the value of count_reservation
-         *
-         * @return  self
-         */ 
-        public function setCount_reservation($count_reservation)
-        {
-                $this->count_reservation = $count_reservation;
-
-                return $this;
-        }
-
-     
+        // Afficher le numéro de réservation
+        echo "Réservation effectuée avec succès !\nNuméro de réservation : " . $num_Reservation;
     }
 
 
 
-?>
+
+
+
+    // //methode qui supprime un employe par son noemp
+    // public function supprEmploye($noemp) : void {
+
+    // }
+
+    // //methode qui mets à jour le salaire d'un amployé
+    // public function updateSalaireEmploye($noemp, $sal) : void {
+
+    // }
+
+
+    /**
+     * Get the value of count_reservation
+     */
+    // public function getCount_reservation()
+    // {
+    //         return $this->count_reservation;
+    // }
+
+    // /**
+    //  * Set the value of count_reservation
+    //  *
+    //  * @return  self
+    //  */ 
+    // public function setCount_reservation($count_reservation)
+    // {
+    //         $this->count_reservation = $count_reservation;
+
+    //         return $this;
+    // }
+
+
+}
