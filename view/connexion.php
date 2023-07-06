87b746c0bbb2f3d205e27d9295b998edac703f36
Aliiel <leila.mouaci@gmail.com>
2023-07-06 09:49:11 +0200

    maj et recup

--- view/connexion.php
+++ view/connexion.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html>
 
@@ -9,32 +7,50 @@
     <link href="./style.connexion.css" rel="stylesheet">
 </head>
 
+<header>
+    <nav>
+   
+            <div id="element3">
+                <h1>Time <br> to <br> Travel</h1>
+            </div>
+        
+       
+    </nav>
+</header>
+
+
 <body>
+    <section>
+
+    
     <form class="form" action="../ctrl/authentification.php" method="POST">
         <div id="titre">
             <h1>Connexion</h1>
         </div>
-        <DIV id = "corps_form">
-        <div class="element">
-            <label for="id">Identifiant : </label>
-            <input id="id" name="id" type="text" placeholder="Entrez votre identifiant" />
-        </div>
-        <div class="element">
-            <label for="mdp">Mot de passe : </label>
-            <input id="mdp" name="mdp" type="password" placeholder="Entrez votre mot de passe" required />
-        </div>
-        <?php
-if (isset($_GET ['error_mdp'])) { ?>
-    <p class = "error"><?php echo $_GET['error_mdp']; ?> </p>
-<?php } ;
-if (isset($_GET ['error_id'])) { ?>
-    <p class = "error"><?php echo $_GET['error_id']; ?> </p>
-<?php } ?>
+
+        <DIV id="corps_form">
+            <div class="element">
+                <label for="id">Identifiant : </label>
+                <input id="id" name="id" type="text" placeholder="Entrez votre identifiant" />
+            </div>
+            <div class="element">
+                <label for="mdp">Mot de passe : </label>
+                <input id="mdp" name="mdp" type="password" placeholder="Entrez votre mot de passe" required />
+            </div>
+            <?php
+            if (isset($_GET['error_mdp'])) { ?>
+                <p class="error"><?php echo $_GET['error_mdp']; ?> </p>
+            <?php };
+            if (isset($_GET['error_id'])) { ?>
+                <p class="error"><?php echo $_GET['error_id']; ?> </p>
+            <?php } ?>
 
 
 
-        <input type="submit" id="submit" name="submit" value="Envoyer" />
+            <input type="submit" id="submit" name="submit" value="Envoyer" />
         </DIV>
     </form>
+</section>
 </body>
+
 </html>
--- view/recherche.php
+++ view/recherche.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,17 +6,57 @@
   <link href="./style.recherche.css" rel="stylesheet">
 </head>
 
+<header>
+    <nav>
+    <div id="el_nav1">
+        <div id="element1">
+
+            <ul>
+                <li>
+                    <a href="#" alt="Toutes nos chambres" target="_blank">Nos chambres</a>
+                    <ul>
+                        <li><a href="#" alt="Chambre Simple" target="_blank">Chambre Simple</a></li>
+                        <li><a href="#" alt="Chambre Double" target="_blank">Chambre Double</a></li>
+                        <li><a href="#" alt="Suite" target="_blank">Suite</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div id="element2">
+            <ul>
+                <li><a href="#" alt="Réserver" target="_blank">Réserver</a></li>
+            </ul>
+        </div>
+    </div>
+            <div id="element3">
+                <h1>Time <br> to <br> Travel</h1>
+            </div>
+        </div>
+        <div id="element4">
+            <ul>
+                <li>
+                    <a href="#" alt="Mon profil" target="_blank">Mon profil</a>
+                    <ul>
+                        <li><a href="#" alt="Mes réservations" target="_blank">Mes réservations</a></li>
+                        <li><a href="#" alt="Mes informations" target="_blank">Mes informations</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
 <body>
   <DIV class="titre">
     <h1> <?php
 
-session_start();
+          session_start();
 
-$bienvenue = "Bonjour et bienvenue " . $_SESSION['Login'] . " dans notre site !";
+          $bienvenue = "Bonjour " . $_SESSION['Login'] . " !";
 
-echo $bienvenue;
+          echo $bienvenue;
 
-?> <br><br>Que souhaitez-vous ? </h1>
+          ?> <br><br>Que recherchez-vous ? </h1>
   </DIV>
 
 
@@ -26,35 +64,36 @@ echo $bienvenue;
   <div class="formulaire">
 
     <div id="chambres">
-    <form action="../ctrl/listChambres.php" method="post">
-      <label for="categorie">Catégorie :</label>
-      <select name="categorie" id="categorie">
-        <option value="">Toutes les catégories</option>
-        <option value="1">Suite</option>
-        <option value="2">Chambre Double</option>
-        <option value="3">Chambre Simple</option>
-      </select>
+      <form action="../ctrl/listChambres.php" method="post">
+        <label for="categorie">Catégorie :</label>
+        <select name="categorie" id="categorie">
+          <option value="">Toutes les catégories</option>
+          <option value="1">Suite</option>
+          <option value="2">Chambre Double</option>
+          <option value="3">Chambre Simple</option>
+        </select>
 
-      <div>
-        <label for="date_debut">Date de début :</label>
-        <input type="date" id="date_debut" name="date_debut" required>
-      </div>
+        <div class = "element_form">
+          <label for="date_debut">Date de début :</label>
+          <input type="date" id="date_debut" name="date_debut" required>
+        </div>
 
-      <div>
-        <label for="date_fin">Date de fin :</label>
-        <input type="date" id="date_fin" name="date_fin" required>
-      </div>
+        <div class = "element_form">
+          <label for="date_fin">Date de fin :</label>
+          <input type="date" id="date_fin" name="date_fin" required>
+        </div>
 
-      <br><br>
-      <input type="submit" id="submit" name="submit" value="Afficher les chambres disponibles">
-    </form>
-  </div>
-  
-  <div id="deconnexion">
-    <form action="connexion.php" method ="post">
-      <input type="submit" name="submit" value="Se déconnecter">
-      <?php session_destroy(); ?>
-    </form>
-  </div>
+        <br><br>
+        <input type="submit" id="submit" name="submit" value="Afficher les chambres disponibles ">
+      </form>
+    </div>
+
+    <div id="deconnexion">
+      <form action="connexion.php" method="post">
+        <input type="submit" name="submit" value="Se déconnecter">
+        <?php session_destroy(); ?>
+      </form>
+    </div>
 </body>
+
 </html>
